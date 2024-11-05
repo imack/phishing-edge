2ff51752eaa756707a4929f9ee478d613e6538f0
Ian MacKinnon <imackinn@gmail.com>
2024-11-04 20:52:15 -0800

    get new harness with option loading to model ready

--- phishing_dataset.py
+++ phishing_dataset.py
@@ -5,42 +5,53 @@ from transformers import DistilBertTokenizer
 import torchvision.transforms as transforms
 
 class PhishingDataset(Dataset):
-    def __init__(self, h5_file, split='train'):
+    def __init__(self, h5_file, required_data, split='train'):
         self.file = h5py.File(h5_file, 'r')
-        self.urls = self.file[f'{split}/urls'][:]
-        self.screenshots = self.file[f'{split}/screenshots'][:]
-        self.html_content = self.file[f'{split}/html_content'][:]
         self.labels = self.file[f'{split}/labels'][:]
         self.tokenizer = DistilBertTokenizer.from_pretrained('distilbert-base-uncased')
-        self.transform = transforms.Compose([
-            transforms.ToPILImage(),
-            transforms.ToTensor(),
-            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-        ])
+
+        if 'image' in required_data:
+            self.screenshots = self.file[f'{split}/screenshots'][:]
+
+            self.transform = transforms.Compose([
+                transforms.ToPILImage(),
+                transforms.ToTensor(),
+                transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+            ])
+
+        self.urls = self.file[f'{split}/urls'][:]
+
+        if 'html_input_ids' in required_data:
+            self.html_content = self.file[f'{split}/html_content'][:]
+
 
     def __len__(self):
         return len(self.labels)
 
     def __getitem__(self, idx):
-        url = self.urls[idx].decode('utf-8')
-        screenshot = self.screenshots[idx]
-        html_content = self.html_content[idx].decode('utf-8')
-        text = f"URL: {url} CONTENT: {html_content}"
-
-        # tokenize the HTML content
-        encoded_html_input = self.tokenizer(
-            text,
-            padding='max_length',
-            truncation=True,
-            max_length=512,
-            return_tensors='pt'
-        )
         label = torch.tensor(self.labels[idx], dtype=torch.long)
+        url = self.urls[idx].decode('utf-8')
+        encoded_html_input = None
+        image = None
 
-        # Preprocess the screenshot
-        image = self.transform(screenshot)
+        if  hasattr(self, 'screenshots'):
+            screenshot = self.screenshots[idx]
+            image = self.transform(screenshot)
 
-        # Tokenize the URL
+        if hasattr(self, 'html_content'):
+            html_content = self.html_content[idx].decode('utf-8')
+            text = f"URL: {url} CONTENT: {html_content}"
+
+            # tokenize the HTML content
+            encoded_html_input = self.tokenizer(
+                text,
+                padding='max_length',
+                truncation=True,
+                max_length=512,
+                return_tensors='pt'
+            )
+
+        # tokenize url contet
         encoded_url_input = self.tokenizer(
             url,
             padding='max_length',
@@ -49,11 +60,13 @@ class PhishingDataset(Dataset):
             return_tensors='pt'
         )
 
-        return {
-            'html_input_ids': encoded_html_input['input_ids'],
-            'html_attention_mask': encoded_html_input['attention_mask'],
+        input_dict = {
+            'html_input_ids': encoded_html_input['input_ids'] if encoded_html_input else None,
+            'html_attention_mask': encoded_html_input['attention_mask'] if encoded_html_input else None,
             'url_input_ids': encoded_url_input['input_ids'].squeeze(),
             'url_attention_mask': encoded_url_input['attention_mask'].squeeze(),
-            'image': image,
+            'image': image if image else None,
             'label': label
         }
+
+        return {k: v for k, v in input_dict.items() if v is not None}
---
+++ test_harness.py
@@ -0,0 +1,73 @@
+from phishing_dataset import PhishingDataset
+import os
+from torch.utils.tensorboard import SummaryWriter
+import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader
+from tqdm.notebook import tqdm
+from utils import evaluate_model
+import torch.optim as optim
+import inspect
+dataset_path = os.path.expanduser("~/transfer/phishing_output.h5")
+
+def test_harness(model, epochs=10, batch_size=8, learning_rate=2e-5):
+    required_data = inspect.signature(model.forward).parameters.keys()
+    train_dataset = PhishingDataset(dataset_path, required_data, split='train')
+    train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    test_dataset = PhishingDataset(dataset_path, required_data, split='test')
+    test_dataloader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+
+    writer = SummaryWriter(model.test_name())
+    device = torch.device('cuda' if torch.cuda.is_available() else 'mps' if torch.backends.mps.is_available() else 'cpu')
+
+    optimizer = optim.AdamW(model.parameters(), lr=learning_rate)
+    criterion = nn.CrossEntropyLoss()
+
+    for epoch in range(epochs):
+        model.train()
+        total_loss = 0
+        progress_bar = tqdm(train_dataloader, desc=f"Epoch {epoch + 1}/{epochs}")
+        for batch in progress_bar:
+            labels = batch['label'].to(device)
+
+            images = batch['image'].to(device) if 'image' in batch else None
+            url_input_ids = batch['url_input_ids'].to(device)
+            url_attention_mask = batch['url_attention_mask'].to(device)
+
+            html_input_ids = batch['html_input_ids'].to(device) if 'html_input_ids' in batch else None
+            html_attention_mask = batch['html_attention_mask'].to(device)  if 'html_attention_mask' in batch else None
+
+            optimizer.zero_grad()
+
+            inputs = {
+                'image': images,
+                'url_input_ids': url_input_ids,
+                'url_attention_mask': url_attention_mask,
+                'html_input_ids': html_input_ids,
+                'html_attention_mask': html_attention_mask
+            }
+
+            filtered_inputs = {k: v for k, v in inputs.items() if v is not None}
+            outputs = model(**filtered_inputs)
+
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+
+            total_loss += loss.item()
+            progress_bar.set_postfix(loss=loss.item())
+
+        avg_loss = total_loss / len(train_dataloader)
+        print(f"Epoch {epoch + 1}/{epochs}, Average Loss: {avg_loss}")
+        writer.add_scalar("Loss/train", avg_loss, epoch)
+
+        # Evaluation after each epoch
+        loss, precision, recall, f1, accuracy = evaluate_model(model, test_dataloader, device, criterion)
+        writer.add_scalar("Loss/test", loss, epoch)
+        writer.add_scalar("Accuracy/test", accuracy, epoch)
+        print(
+            f"Epoch {epoch + 1}/{epochs}, Loss: {loss:.4f}, Precision: {precision:.4f}, Recall: {recall:.4f}, F1 Score: {f1:.4f}, Accuracy: {accuracy:.4f}")
+        writer.flush()
+    writer.close()
+
+    return model
