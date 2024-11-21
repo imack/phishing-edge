a42d312514c0452ba909983bd6228f598dcd0a90
Ian MacKinnon <imackinn@gmail.com>
2024-11-21 07:42:23 -0800

    add a few variations of bert for urls

---
+++ classifiers/basic_url_skinny_bert_classifier.py
@@ -0,0 +1,22 @@
+import torch.nn as nn
+from transformers import DistilBertModel
+
+class BasicUrlBertClassifier(nn.Module):
+    def __init__(self):
+        super(BasicUrlBertClassifier, self).__init__()
+
+        self.bert = DistilBertModel.from_pretrained('distilbert-base-uncased')
+        # Use only the first 2 layers
+        self.bert.transformer.layer = self.bert.transformer.layer[:2]
+
+        self.classifier = nn.Linear(self.bert.config.hidden_size, 2)
+
+    def forward(self, url_input_ids, url_attention_mask):
+        bert_outputs = self.bert(input_ids=url_input_ids, attention_mask=url_attention_mask)
+        url_features = bert_outputs.last_hidden_state[:, 0, :]
+
+        logits = self.classifier(url_features)
+        return logits
+
+    def test_name(self):
+        return 'basic_url_skinny_bert'
--- test_harness.py
+++ test_harness.py
@@ -52,7 +52,7 @@ def test_harness(model, local_dataset=None, epochs=10, batch_size=8, learning_ra
         writer.add_scalar("Loss/test", loss, epoch)
         writer.add_scalar("Accuracy/test", accuracy, epoch)
         print(
-            f"Epoch {epoch + 1}/{epochs}, Test Loss: {loss:.4f}, Precision: {precision:.4f}, Recall: {recall:.4f}, F1 Score: {f1:.4f}, Accuracy: {accuracy:.4f}")
+            f"Epoch {epoch + 1}/{epochs}, Dev Loss: {loss:.4f}, Precision: {precision:.4f}, Recall: {recall:.4f}, F1 Score: {f1:.4f}, Accuracy: {accuracy:.4f}")
         writer.flush()
     writer.close()
 
