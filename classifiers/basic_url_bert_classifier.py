be3105baff9671e1b3b54f328934b5f0d5d37ae4
Ian MacKinnon <imackinn@gmail.com>
2024-11-21 07:40:04 -0800

    basic url bert

--- classifiers/basic_url_bert_classifier.py
+++ classifiers/basic_url_bert_classifier.py
@@ -1,18 +1,13 @@
 import torch.nn as nn
 from transformers import DistilBertModel
 
-class BasicBertClassifier(nn.Module):
+class BasicUrlBertClassifier(nn.Module):
     def __init__(self):
-        super(BasicBertClassifier, self).__init__()
+        super(BasicUrlBertClassifier, self).__init__()
 
         self.bert = DistilBertModel.from_pretrained('distilbert-base-uncased')
 
-        self.classifier = nn.Sequential(
-            nn.Linear(self.bert.config.hidden_size, 256),
-            nn.ReLU(),
-            nn.Dropout(0.3),
-            nn.Linear(256, 2)
-        )
+        self.classifier = nn.Linear(self.bert.config.hidden_size, 2)
 
     def forward(self, url_input_ids, url_attention_mask):
         bert_outputs = self.bert(input_ids=url_input_ids, attention_mask=url_attention_mask)
