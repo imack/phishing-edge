a1d095f011f487991f8ace8e78fc6fb17ce3acd1
Ian MacKinnon <imackinn@gmail.com>
2024-11-28 10:01:19 -0800

    cleanup test harness

--- test_harness.py
+++ test_harness.py
@@ -10,10 +10,6 @@ import torch.optim as optim
 import inspect
 
 def chunk_sequence(sequence, chunk_size=512):
-    """
-    Chunk a sequence into fixed-sized segments of `chunk_size`.
-    Returns the chunks as a tensor of shape (num_chunks, chunk_size).
-    """
     chunks = [sequence[i:i + chunk_size] for i in range(0, len(sequence), chunk_size)]
     return torch.stack([torch.tensor(chunk) for chunk in chunks])
 
@@ -23,17 +19,13 @@ def custom_collate_fn(batch, chunk_size=512):
 
     max_num_chunks = 0
 
-    # Process each item in the batch
     for item in batch:
-        # Chunk the input sequence
         chunks = chunk_sequence(item['html_input_ids'], chunk_size)
         max_num_chunks = max(max_num_chunks, chunks.size(0))
 
-        # Append chunks and labels
         all_chunks.append(chunks)
         labels.append(item['label'])
 
-    # Pad all chunks to ensure batch consistency
     padded_chunks = []
     for chunks in all_chunks:
         num_chunks = chunks.size(0)
@@ -61,9 +53,9 @@ def test_harness(model, local_dataset=None, epochs=10, batch_size=8, learning_ra
     if torch.cuda.is_available():
         num_workers = 6
     train_dataset = PhishingDataset(required_data, split='train', local_file_path=local_dataset)
-    train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, pin_memory=True, num_workers=num_workers, collate_fn=custom_collate_fn)
+    train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, pin_memory=True, num_workers=num_workers)
     test_dataset = PhishingDataset(required_data, split='dev', local_file_path=local_dataset)
-    test_dataloader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False, pin_memory=True, num_workers=num_workers, collate_fn=custom_collate_fn)
+    test_dataloader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False, pin_memory=True, num_workers=num_workers)
 
     writer = SummaryWriter(f"runs/{model.test_name()}")
     device = torch.device('cuda' if torch.cuda.is_available() else 'mps' if torch.backends.mps.is_available() else 'cpu')
@@ -74,13 +66,13 @@ def test_harness(model, local_dataset=None, epochs=10, batch_size=8, learning_ra
     for epoch in range(epochs):
         model.train()
         total_loss = 0
-        progress_bar = tqdm(train_dataloader, desc=f"Epoch {epoch + 1}/{epochs}", leave=True, miniters=len(train_dataloader) // 10)
+        progress_bar = tqdm(train_dataloader, desc=f"Epoch {epoch + 1}/{epochs}")
         for batch in progress_bar:
             optimizer.zero_grad()
 
             labels = batch['label'].to(device)
             filtered_inputs = get_filtered_inputs(batch)
-            class_probs, windowed_scores, summarized_embeddings, attention_weights = model(**filtered_inputs)
+            class_probs = model(**filtered_inputs)
 
             loss = criterion(class_probs, labels)
             loss.backward()
@@ -92,6 +84,8 @@ def test_harness(model, local_dataset=None, epochs=10, batch_size=8, learning_ra
         avg_loss = total_loss / len(train_dataloader)
         print(f"Epoch {epoch + 1}/{epochs}, Average Loss: {avg_loss}")
         writer.add_scalar("Loss/train", avg_loss, epoch)
+
+        torch.save(model.state_dict(), f"models/{model.test_name()}_phishing_classifier_epoch_{epoch}.pt")
 
         # Evaluation after each epoch
         loss, precision, recall, f1, accuracy = evaluate_model(model, test_dataloader, device, criterion)
