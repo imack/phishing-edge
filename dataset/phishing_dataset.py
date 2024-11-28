0ff57a205e449982e56d0de0235ce6d4b32299db
Ian MacKinnon <imackinn@gmail.com>
2024-11-28 10:02:25 -0800

    move images to use pre numpy'd things

--- dataset/phishing_dataset.py
+++ dataset/phishing_dataset.py
@@ -29,16 +29,7 @@ class PhishingDataset(Dataset):
 
         self.file = h5py.File(local_file_path, 'r')
         self.labels = self.file[f'{split}/labels'][:]
-
-        if 'image' in required_data:
-            self.screenshots = self.file[f'{split}/screenshots'][:]
-
-            self.transform = transforms.Compose([
-                transforms.ToPILImage(),
-                transforms.ToTensor(),
-                transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-            ])
-
+        self.images = self.file[f'{split}/image'][:]
         self.urls = self.file[f'{split}/urls'][:]
 
         if 'html_input_ids' in required_data:
@@ -52,6 +43,18 @@ class PhishingDataset(Dataset):
 
             html_attention_masks = [torch.tensor(item, dtype=torch.float) for item in html_attention_masks]
             self.html_attention_masks = html_attention_masks
+
+        if 'longformer_input_ids' in required_data:
+            longformer_input_ids = self.file[f'{split}/longformer_input_ids'][:]
+
+            longformer_input_ids = [torch.tensor(item, dtype=torch.long) for item in longformer_input_ids]
+            self.longformer_input_ids = longformer_input_ids
+
+        if 'longformer_attention_mask' in required_data:
+            longformer_attention_mask = self.file[f'{split}/longformer_attention_masks'][:]
+
+            longformer_attention_mask = [torch.tensor(item, dtype=torch.float) for item in longformer_attention_mask]
+            self.longformer_attention_mask = longformer_attention_mask
 
         if 'url_input_ids' in required_data:
             self.url_input_ids = self.file[f'{split}/url_input_ids'][:]
@@ -69,21 +72,17 @@ class PhishingDataset(Dataset):
         return len(self.labels)
 
     def __getitem__(self, idx):
-        label = torch.tensor(self.labels[idx], dtype=torch.long)
-        url = self.urls[idx].decode('utf-8')
-        encoded_html_input = None
-        image = None
-
-        if hasattr(self, 'screenshots'):
-            screenshot = self.screenshots[idx]
-            image = self.transform(screenshot)
+        label = torch.tensor(self.labels[idx], dtype=torch.float32)
 
         input_dict = {
             'html_input_ids': self.html_input_ids[idx] if 'html_input_ids' in self.required_data else None,
             'html_attention_mask': self.html_attention_masks[idx] if 'html_input_ids' in self.required_data else None,
+            'longformer_input_ids': self.longformer_input_ids[idx] if 'longformer_input_ids' in self.required_data else None,
+            'longformer_attention_mask': self.longformer_attention_mask[idx] if 'longformer_input_ids' in self.required_data else None,
             'url_input_ids': self.url_input_ids[idx] if 'url_input_ids' in self.required_data else None,
             'url_attention_mask': self.url_attention_masks[idx] if 'url_attention_mask' in self.required_data else None,
-            'image': image if image is not None and image.numel() > 0 else None,
+            'url': self.urls[idx].decode('utf-8') if 'url' in self.required_data else None,
+            'image': self.images[idx] if 'image' in self.required_data else None,
             'label': label
         }
 
